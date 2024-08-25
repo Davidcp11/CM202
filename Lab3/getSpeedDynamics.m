5ddee702d64f2c15b7cd113522c4b08443713b8f
David Costa Pereira <pereiracostadavid@gmail.com>
2024-08-25 16:14:20 -0300

    Lab2, 3 e tutorial de optimizacao

---
+++ Lab3/getSpeedDynamics.m
@@ -0,0 +1,8 @@
+function dynamics = getSpeedDynamics(servo)
+% dynamics = getSpeedDynamics(servo) obtains the motor transfer function 
+% given the servo parameters, stored in the struct servo.
+
+% Implement 
+% dynamics = tf(...)
+
+end
